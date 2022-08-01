f1be439a8007df3b959b168a4257c3f1e5f2e371
SergioFVS <sergiofvs@outlook.com>
2022-07-31 21:51:56 -0300

    Implement local api
    - Add json-server to the project
    - Move animals data from component to a db.json file
    - Import and use HttpClient module
    - Update list service to make http requests to the api

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Animal } from '../animal';
+import { API_URL } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ListService {
-  constructor() {}
+  private base_url: string = `${API_URL}animals`;
 
-  remove(animals: Animal[], animal: Animal) {
-    return animals.filter((a) => animal.name !== a.name);
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Animal[]> {
+    return this.http.get<Animal[]>(this.base_url);
+  }
+
+  post(animal: Animal): Observable<Animal> {
+    return this.http.post<Animal>(this.base_url, animal);
+  }
+
+  remove(animal_id: number) {
+    return this.http.delete<number>(`${this.base_url}/${animal_id}`);
   }
 }
